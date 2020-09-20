709e763c4ac5a2d0b6d1ee6580c95342bc73cdf4
Juan Mireles II <jmireles2dev@gmail.com>
2020-09-20 18:33:12 -0500

    Added name/class info header

--- module03/programmingChallenges/hotRod/hotRod.cpp
+++ module03/programmingChallenges/hotRod/hotRod.cpp
@@ -1,3 +1,17 @@
+/* Filename: car.h
+   Title:  Car Accelerating/Braking
+   
+   Name: Juan Mireles II
+   Course: COSC 1337.V01 - Programming Fundamentals II
+   Term: Fall 2020
+   Institution: South Texas College
+   Instructor: Mr. Joseph Perez
+   Due: Sunday, September 20, 2020
+   Notes: Car Class via video provided modified
+          This file definds a Car class and includes a client program
+          that uses a Car object to simulate accelerating and breaking.
+*/
+
 #include <iostream>
 #include "car.h"
 using namespace std;
