c655c63da3b964a9fe5c14975572402a7cf8e526
Juan Mireles II <jmireles2dev@gmail.com>
2020-10-11 05:56:53 -0500

    Added header info, adjusted spacing after algorithm

--- module04/challenge2/lottery.cpp
+++ module04/challenge2/lottery.cpp
@@ -1,10 +1,32 @@
+/* Filename: lottery.cpp
+   Title:  Lucky Number Lottery Determinator
+   
+   Name: Juan Mireles II
+   Course: COSC 1337.V01 - Programming Fundamentals II
+   Term: Fall 2020
+   Institution: South Texas College
+   Instructor: Mr. Joseph Perez
+   Due: Sunday, October 4, 2020
+   Notes: (This next problem is not in this book but there is an example of 
+          using the binary search with the array class)
+
+          Use the class array to store the numbers of 10 five-digit "lucky" 
+          combinations tickets.  Write a program that initializes the class 
+          array with the numbers of these 10 lucky combination tickets and then
+          lets the player enter this week's winning five-digit number. The
+          program should perform a binary search through the list of the
+          player's numbers and report whether or not one of the tickets is a
+          winner this week. Here are the numbers:
+
+          26791 26792 33445 55555 62483 77777 79422 85647 93121
+*/
+
 #include <iostream>
 #include <iomanip>
 #include <array>
 #include <string>
 #include <algorithm> // Contains sort and binary_search
-
-#include "validation.h"
+#include "Validation.h"
 using namespace std;
 
 int main()
