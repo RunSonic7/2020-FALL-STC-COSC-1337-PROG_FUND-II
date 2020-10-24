a9d85532482b092030b2827f55f0f34a8a3b8895
Juan Mireles II <jmireles2dev@gmail.com>
2020-10-23 23:25:23 -0500

    Program Complete

--- module05/challenge1/GradeBook.h
+++ module05/challenge1/GradeBook.h
@@ -1,8 +1,20 @@
+/* Filename: GradeBook.h
+   Title: Implementation File for GradeBook Class
+
+   Name: Juan Mireles II
+   Course: COSC 1337.V01 - Programming Fundamentals II
+   Term: Fall 2020
+   Institution: South Texas College
+   Instructor: Mr. Joseph Perez
+   Due: Sunday, October 18, 2020
+   Notes: Implementation File for GradeBook Class
+*/
 #include <iostream>
 
 class GradeBook
 {
 private:
+    int* testScores;
 
     int size;
     std::string userInput;
@@ -14,30 +26,45 @@ private:
 
 public:
 
-    int* testScores;
-
     ~GradeBook()
     {
         delete[] testScores;
         testScores = 0;
     }
 
-    void validateInput(std::string input)
-    {
-        // Check if a positive number
-        isNumber = checkIfNumber(input);
+    void validateInput(std::string);
+    bool checkIfNumber(std::string);
+    void displayData();
+    void calculateAverage();
 
-        if (isNumber == true)
-        { // Number has already been casted
-            if (userInputCast > 0) {
-                isValidInput = true;
+    void modifyArray(int index, int grade)
+    {  *(testScores + index) = grade;  }
 
-            }
-            else
-            {
-                std::cout << "You did not input a valid entry. Please try again.\n" << std::endl;
-                isValidInput = false;
-            }
+    int getGradeBookSize()
+    {  return size;  }
+
+    void createGradebookArray()
+    {  testScores = new int[size];  }
+
+    void setSize()
+    {  size = userInputCast;  }
+
+    bool getIsValidInput()
+    {  return isValidInput;  }
+
+    void castToInt(std::string input)
+    {  userInputCast = stoi(input);  }
+};
+
+void GradeBook::validateInput(std::string input)
+{
+    // Check if a positive number
+    isNumber = checkIfNumber(input);
+
+    if (isNumber == true)
+    { // Number has already been casted
+        if (userInputCast > 0) {
+            isValidInput = true;
 
         }
         else
@@ -46,72 +73,47 @@ public:
             isValidInput = false;
         }
     }
-
-    int getGradeBookSize()
+    else
     {
-        return size;
+        std::cout << "You did not input a valid entry. Please try again.\n" << std::endl;
+        isValidInput = false;
     }
+}
 
-    void createGradebookArray()
+bool GradeBook::checkIfNumber(std::string input)
+{
+    // Check each character. Once false, loop breaks
+    for (int i = 0; i < input.length(); i++)
     {
-        testScores = new int[size];
-    }
-
-    void setSize()
-    {
-        size = userInputCast;
-    }
-    
-    bool getIsValidInput()
-    {
-        return isValidInput;
-    }
-
-    bool checkIfNumber(std::string input)
-    {
-        // Check each character. Once false, loop breaks
-        for (int i = 0; i < input.length(); i++)
+        if (isdigit(input[i]) == false)
         {
-            if (isdigit(input[i]) == false)
-            {
-                return false;
-            }
+            return false;
         }
-        castToInt(input); // Since number, cast to int type
-        return true;
-    };
-
-    void displayData()
-    {
-        calculateAverage();
-        for (int index = 0; index < size; index++)
-        {
-            std::cout << *(testScores + index) << std::endl;
-        }
-        std::cout << size << std::endl;
-        std::cout << "The average is: " << average << std::endl;
     }
-
-    void calculateAverage()
-    {
-        for (int index = 0; index < size; index++)
-        {
-            double sum;
-            sum += *(testScores + index);
-            average = sum / size;
-        }
-
-    }
-    void castToInt(std::string input)
-    {
-        userInputCast = stoi(input);
-    }
+    castToInt(input); // Since number, cast to int type
+    return true;
 };
 
-// GradeBook::GradeBook(/* args */)
-// {
-// }
+void GradeBook::displayData()
+{
+    calculateAverage();
+    for (int index = 0; index < size; index++)
+    {
+        std::cout << *(testScores + index) << std::endl;
+    }
+    std::cout << size << std::endl;
+    std::cout << "The average is: " << average << std::endl;
+}
 
+void GradeBook::calculateAverage()
+{
+    for (int index = 0; index < size; index++)
+    {
+        double sum;
+        sum += *(testScores + index);
+        average = sum / size;
+    }
+}
 
 // Sources:
 // Destructor: https://docs.microsoft.com/en-us/cpp/cpp/destructors-cpp?view=vs-2019
--- module05/challenge1/main.cpp
+++ module05/challenge1/main.cpp
@@ -1,27 +1,44 @@
-/* Filename:
-   Title:
+/* Filename: main.cpp
+   Title: Test Scores #1
 
    Name: Juan Mireles II
    Course: COSC 1337.V01 - Programming Fundamentals II
    Term: Fall 2020
    Institution: South Texas College
    Instructor: Mr. Joseph Perez
-   Due:
-   Notes:
+   Due: Sunday, October 18, 2020
+   Notes: Write a program that dynamically allocates a built-in array large
+          enough to hold a user-defined number of test scores. (Ask the user how
+          many grades will be entered and use a dynamic array to store the
+          numbers.) Once all the scores are entered, the array should be passed
+          to a function that calculates the average score. The program should
+          display the scores and average. Use pointer notation rather than array
+          notation whenever possible. (Input Validation: Do not accept negative
+          numbers for test scores.) Make it a class, call it something like
+          gradeholder.  You will need a destructor because you are using dynamic
+          arrays.
+    Credit: ASCII Art Generator http://patorjk.com/software/taag/ 
 */
-#include "GradeBook.h"
+
 #include <iostream>
+#include "GradeBook.h"
 using namespace std;
+
+void displayTitle();
 
 int main()
 {
+    // Variables
     GradeBook gradeBook;
     string size;
     string grade;
 
+    displayTitle(); // Title ASCII Art
+
     do
     {
-        cout << "How many test scores do you have to input? \n";
+        // Ask User 
+        cout << "How many test scores do you have to input?  ";
         cin >> size;
         gradeBook.validateInput(size);
 
@@ -31,13 +48,12 @@ int main()
     gradeBook.setSize();    // Set size of gradebook
     gradeBook.createGradebookArray();
 
-    // As user to enter test scores;
-    for (int index = 0; index < gradeBook.getGradeBookSize(); index++) {
-
+    // As user to enter test scores, validate, reask if invalid, add to dynamic array.
+    for (int index = 0; index < gradeBook.getGradeBookSize(); index++)
+    {
         do
         {
             cout << "Enter grade number " << (index + 1) << ": ";
-
             cin >> grade;
 
             // Validate if grade is valid input
@@ -46,12 +62,23 @@ int main()
         } while (gradeBook.getIsValidInput() == false);
 
         // Store in testScores dynamic array.
-        gradeBook.testScores[index] = stoi(grade);
-
+        gradeBook.modifyArray(index, stoi(grade));
     }
 
     // Display grades and average to user;
     gradeBook.displayData();
 
+    // Delete allocated memory
     gradeBook.~GradeBook();
+}
+
+void displayTitle()
+{
+    cout << " _____  _                _____                   _       ______                _\n"
+        "|_   _|| |              |  __ \\                 | |      | ___ \\              | |   \n"
+        "  | |  | |__    ___     | |  \\/ _ __   __ _   __| |  ___ | |_/ /  ___    ___  | | __\n"
+        "  | |  | '_ \\  / _ \\    | | __ | '__| / _` | / _` | / _ \\| ___ \\ / _ \\  / _ \\ | |/ /\n"
+        "  | |  | | | ||  __/    | |_\\ \\| |   | (_| || (_| ||  __/| |_/ /| (_) || (_) ||   < \n"
+        "  \\_/  |_| |_| \\___|     \\____/|_|    \\__,_| \\__,_| \\___|\\____/  \\___/  \\___/ |_|\\_\\"
+        "\n\n\n";
 }
