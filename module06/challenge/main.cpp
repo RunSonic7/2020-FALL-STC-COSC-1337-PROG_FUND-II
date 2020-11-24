bb340dcfddd0ddf9d94a8f67f722afc95da1f739
Juan Mireles II <jmireles2dev@gmail.com>
2020-11-23 20:33:31 -0600

    Added module06 challenge empty files

---
+++ module06/challenge/main.cpp
@@ -0,0 +1,11 @@
+/* Filename: main.cpp
+   Title:  Driver File for Rational Class Challenge
+   
+   Name: Juan Mireles II
+   Course: COSC 1337.V01 - Programming Fundamentals II
+   Term: Fall 2020
+   Institution: South Texas College
+   Instructor: Mr. Joseph Perez
+   Due: Sunday, November 1, 2020
+   Notes: Add instructions here
+*/
